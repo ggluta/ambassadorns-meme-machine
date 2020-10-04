e56135af3a1af5740437cd5cda8a9bab9e0ae7e2
gg <luta.george.gabriel@gmail.com>
2020-10-04 17:33:03 +0200

    project init

---
+++ server/index.js
@@ -0,0 +1,46 @@
+var path = require('path'); 
+var express = require('express');
+var logger = require('morgan');
+var cors = require('cors');
+var app = express();
+
+
+// nicely log http requests to terminal
+app.use(logger('dev'));
+
+// serve folder with userContent statically
+app.use('/api/uploads/', express.static('uploads'));
+
+// api subrouter
+var router = express.Router();
+// default data
+const memes  =  [{image:"http://localhost:8000/api/uploads/default.png", text: "a funny joke"}, {image:"http://localhost:8000/api/uploads/default.png", text: "the punchline"}, {image:"http://localhost:8000/api/uploads/default.png", text: "the applause"}];
+// json body parser 
+router.use(express.json())
+// allow cors so frontend dev can access api
+router.use(cors())
+
+// retrieve a list of very fine memes
+router.get('/memes', function(req, res) {
+    res.json(memes)
+});
+
+// logs request and returns request body
+router.post('/log', function(req, res) {
+    console.log((req.body))
+    res.json(req.body)
+});
+
+// add the router to the api subroute
+app.use('/api', router)
+
+// serve static frontend build
+app.use( express.static('build'));
+
+// if other paths dont match serve index.html for single page app routing 
+app.use(function(req, res) {
+    res.sendFile(path.join(__dirname,'../build/index.html'));
+});
+
+app.listen(8000)
+
